4c8c2e095699160a4c3d6123926f56d16bbeffaa
Michael Peer <michaelpeer2004@gmail.com>
2021-04-01 09:52:06 +0300

    G

--- src/cmps/task/task-preview.vue
+++ src/cmps/task/task-preview.vue
@@ -109,7 +109,8 @@
               @mouseout="dueDateIcon = 'clock'"
               @click.stop="isDoneToggle"
             >
-
+            
+              <!-- <font-awesome-icon class="due-date-icon" :icon="dateIcon" /> -->
               <font-awesome-icon
                 class="due-date-icon"
                 :icon="['far', dueDateIcon]"
@@ -173,6 +174,7 @@ export default {
       labels: this.task.labelIds,
       isEdit: false,
       dueDateIcon: "clock",
+      icon: "clock",
     };
   },
   computed: {
@@ -204,6 +206,23 @@ export default {
         isToday: this.$moment().isSame(this.task.dueDate, "day"),
         isDone: this.task.isDone,
       };
+    },
+    dateIcon() {
+      let icon = [];
+      switch (this.dueDateIcon) {
+        case "notDone":
+          icon = ["far", "square"];
+          break;
+        case "done":
+          icon = ["far", "check-square"];
+          break;
+        case "clock":
+          icon = ["far", "clock"];
+          break;
+        default:
+          break;
+      }
+      return icon;
     },
   },
   methods: {
